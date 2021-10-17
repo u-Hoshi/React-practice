2e589ceb1fb0fcee4d41a43a152227cf9db4c00b
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-17 23:35:47 +0900

    readmeの編集

--- react-router-dom-exact/src/App.tsx
+++ react-router-dom-exact/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
               <Link to='/'>Home</Link>
             </li>
             <li>
-              <Link to='/about'>AboutTop</Link>
+              <Link to='/about'>About</Link>
             </li>
             <li>
               <Link to='/about/1'>About1</Link>
@@ -20,7 +20,7 @@ function App() {
         </nav>
         <Switch>
           <Route exact path='/about'>
-            <h1>aboutTop</h1>
+            <h1>about</h1>
           </Route>
           <Route path='/about/1'>
             <h1>about1</h1>
