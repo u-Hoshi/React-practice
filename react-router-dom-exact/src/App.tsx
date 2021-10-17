51af3200855c1e18e122826aa470d50d80125f1c
u-Hoshi <yuta000707@yahoo.co.jp>
2021-10-17 23:28:59 +0900

    router-dom完成

--- react-router-dom-exact/src/App.tsx
+++ react-router-dom-exact/src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>AboutTop</Link>
+            </li>
+            <li>
+              <Link to='/about/1'>About1</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path='/about'>
+            <h1>aboutTop</h1>
+          </Route>
+          <Route path='/about/1'>
+            <h1>about1</h1>
+          </Route>
+          <Route path='/'>
+            <h1>top</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
