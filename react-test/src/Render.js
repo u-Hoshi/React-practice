71ae5142cf851e660ec7288e5cdfb8c0418f0c67
u-Hoshi <yuta000707@yahoo.co.jp>
2021-11-25 22:59:18 +0900

    コンポーネント作成

---
+++ react-test/src/Render.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Render = () => {
+  return (
+    <div>
+      <h1>hoge</h1>
+      <input type='text' />
+      <button>click1</button>
+      <button>click2</button>
+      <p>hoge</p>
+      <span>@foo</span>
+    </div>
+  );
+};
+
+export default Render;
