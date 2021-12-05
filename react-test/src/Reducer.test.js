0900225c6a45472acdc56a6fd87bad66da68593e
u-Hoshi <yuta000707@yahoo.co.jp>
2021-12-05 22:51:02 +0900

    reducerテスト(2)

--- react-test/src/Reducer.test.js
+++ react-test/src/Reducer.test.js
@@ -1,5 +1,6 @@
 import reducer, {
   increment,
+  incrementByAmount,
   // incrementByAmount,
 } from './features/customCounter/customCounterSlice.js';
 
@@ -31,6 +32,35 @@ describe('Reducer of ReduxToolKit', () => {
       const action = { type: increment.type };
       const state = reducer(initialState, action);
       expect(state.value).toEqual(10001);
+    });
+  });
+  describe('incrementByAmount action', () => {
+    let initialState = {
+      mode: 0,
+      value: 1,
+    };
+    it('should increment by payload value with mode 0', () => {
+      const action = { type: incrementByAmount.type, payload: 3 };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(4);
+    });
+    it('should increment by payload value with mode 0', () => {
+      let initialState = {
+        mode: 1,
+        value: 1,
+      };
+      const action = { type: incrementByAmount.type, payload: 3 };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(301);
+    });
+    it('should increment by payload value with mode 0', () => {
+      let initialState = {
+        mode: 2,
+        value: 1,
+      };
+      const action = { type: incrementByAmount.type, payload: 3 };
+      const state = reducer(initialState, action);
+      expect(state.value).toEqual(30001);
     });
   });
 });
