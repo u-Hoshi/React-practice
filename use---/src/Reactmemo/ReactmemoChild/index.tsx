697601ca7211165cb47d98d133ca18afe7c2f5f8
u-Hoshi <yuta000707@yahoo.co.jp>
2021-12-29 09:10:31 +0900

    useCallbackなど

--- use---/src/Reactmemo/ReactmemoChild/index.tsx
+++ use---/src/Reactmemo/ReactmemoChild/index.tsx
@@ -2,9 +2,16 @@ import { memo, VFC } from 'react';
 
 type Props = {
   open?: boolean;
+  onClickClose: () => void;
 };
 
-export const ReactmemoChild: VFC<Props> = memo(({ open }) => {
+export const ReactmemoChild: VFC<Props> = memo(({ open, onClickClose }) => {
+  // React.memo(コンポーネントのメモ化)
   console.log('再レンダリング!!!');
-  return <>{open && <p>child</p>}</>;
+  return (
+    <>
+      {open && <p>child</p>}
+      <button onClick={onClickClose}>閉じる</button>
+    </>
+  );
 });
--- use---/src/Reactmemo/index.tsx
+++ use---/src/Reactmemo/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { ReactmemoChild } from './ReactmemoChild';
 
 export const Reactmemo = () => {
@@ -8,11 +8,23 @@ export const Reactmemo = () => {
   const onClickButton1 = () => setOpen(!open);
   const onClickButton2 = () => setNotOpen(!notOpen);
 
+  // アロー関数で作成した関数はレンダリング毎の毎回新規作成されているとみなされる
+  // →それによってpropsに変更があったと子コンポーネントが認識し、毎回レンダリングされることになる
+  // これを防ぐのがuseCallback(関数のメモ化)
+  // 子コンポーネントに渡さない関数にuseCallbackを使用しても意味ない
+  const onClickClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  // useMemo(変数のメモ化)
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const temp = useMemo(() => 1 + 3, []);
+
   return (
     <div style={{ textAlign: 'center' }}>
       <button onClick={onClickButton1}>Childを表示</button>
       <button onClick={onClickButton2}>Childとは関係ない</button>
-      <ReactmemoChild open={open} />
+      <ReactmemoChild open={open} onClickClose={onClickClose} />
     </div>
   );
 };
